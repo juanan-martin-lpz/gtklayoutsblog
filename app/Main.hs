5790ad18b281bd09504e42b0a6c52ccdc575f87a
Juan Martin <juanan.martin.lpz@gmail.com>
2020-02-10 21:18:16 +0100

    Ejemplo basico, basico

--- app/Main.hs
+++ app/Main.hs
@@ -3,28 +3,58 @@
 module Main where
     
     import qualified GI.Gtk as Gtk
+    import qualified GI.Gtk.Enums as Enums
+    
     import Data.GI.Base
+    import Data.Text
+    import Control.Monad.IO.Class
+
+    -- creamos la ventana
+    createWindow ::  MonadIO m => Text -> m Gtk.Window
+    createWindow title = do
+        win <- new Gtk.Window [ #title := title ]
+        liftIO $ return win
+
+    -- creamos un Box
+    createBox :: MonadIO m => Enums.Orientation -> m Gtk.Box
+    createBox orientation = do
+        box <- new Gtk.Box [ #orientation := orientation]
+        liftIO $ return box   
+    
+    -- creamos componentes
+    createInputArea :: MonadIO m => Gtk.Box -> m Gtk.Box
+    createInputArea box = do
+        label <- new Gtk.Label [ #label := "Sample Text"]
+        textbox <- new Gtk.TextView []
+
+        #add box label
+        #add box textbox
+        
+        liftIO $ return box
+
+
+    createButtons :: MonadIO m => Gtk.Box -> m Gtk.Box
+    createButtons box = do
+        aceptar <- new Gtk.Button [ #label := "Aceptar"]
+        cancelar <- new Gtk.Button [ #label := "Cancelar"]
+
+        #add box aceptar
+        #add box cancelar
+        liftIO $ return box
+
 
     main :: IO ()
     main = do
         Gtk.init Nothing
 
-        win <- new Gtk.Window [ #title := "Hi there" ]
-
+        win <- createWindow "Layouts Sample"
         on win #destroy Gtk.mainQuit
 
-        -- Forma alternativa
-        -- Gtk.onWidgetDestroy win $ do Gtk.mainQuit
+        box <- createBox Enums.OrientationVertical
+        createBox Enums.OrientationVertical >>= createInputArea >>= #add box
+        createBox Enums.OrientationHorizontal >>= createButtons >>= #add box
 
-        button <- new Gtk.Button [ #label := "Click me" ]
-
-        on button #clicked (set button [ #sensitive := False, #label := "Thanks for clicking me" ])
-
-        -- Version alternativa
-        -- Gtk.onButtonClicked button $ do
-        --     button `set ` [ #sensitive := False, #label := "Thanks for clicking me" ]
-
-        #add win button
+        #add win box
 
         #showAll win
 
